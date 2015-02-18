35a815d7adf43f80a41bc63ca5f90203839f3d9a
tucker mayo & Heather Anderson <student@epicodus.com>
2015-02-18 11:48:30 -0800

    add jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,45 @@
 var BankAccount = {
+  name: "",
   balance: 0,
+
   withdraw: function(amount) {
     this.balance = this.balance - amount;
   },
 
   deposit: function(amount) {
     this.balance = this.balance + amount;
+
   }
 };
 
-var myBankAccount = Object.create(BankAccount);
-myBankAccount.balance;
-myBankAccount.deposit(20);
-myBankAccount.balance;
 
-var yourBankAccount = Object.create(BankAccount);
-yourBankAccount.balance;
-yourBankAccount.withdraw(1000000);
-yourBankAccount.balance;
+// ------------------------------------------
+
+$(document).ready(function() {
+  $("form#new-account").submit(function(event) {
+    event.preventDefault();
+
+    var customerName = $("input#customer-name").val();
+    var intialDeposit = $("input#initial-deposit").val();
+    // var deposit = $("input#deposit").val();
+    // var withdraw = $("input#withdraw").val();
+    var newAccount = Object.create(BankAccount);
+    newAccount.balance = initialDeposit;
+    newAccount.customerName = customerName;
+
+    $("ul#show-contacts").append("<li><span class='newAccount'>" + newAccount.customerName + " " + newAccount.balance + "</span></li>");
+
+
+    $("input#customer-name").val("");
+    $("input#initial-deposit").val();
+    $("input#deposit").val();
+    $("input#withdraw").val();
+
+
+
+    $("#new-account").last().click(function() {
+      $("#show-contacts").show();
+      $(".newAccount").text(newAccount);
+    });
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -4,8 +4,8 @@ describe("BankAccount", function() {
     var testBankAccount = Object.create(BankAccount);
     testBankAccount.deposit(10);
     expect(testBankAccount.balance).to.equal(10);
+    });
   });
-});
 
   describe("withdraw", function() {
     it("subtracts the amount from the balance", function() {
